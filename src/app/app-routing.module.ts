cd7e274c2d12e2d0e4f6fe6f4c325c2f786d2e65
Georgia Stuart <georgia.stuart@gmail.com>
2019-03-18 21:02:23 -0700

    Added components for blog home and blog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BlogPostComponent } from './blog-post/blog-post.component';
+import {BlogComponent} from './blog/blog.component';
+import {BlogHomeComponent} from './blog-home/blog-home.component';
 
 const routes: Routes = [
   {
-    path: 'blog/:year/:month/:day/:slug',
-    component: BlogPostComponent
+    path: 'blog',
+    component: BlogComponent,
+    children: [
+      {
+        path: '',
+        component: BlogHomeComponent
+      },
+      {
+        path: ':year/:month/:day/:slug',
+        component: BlogPostComponent
+      }
+    ]
   }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgxMdModule } from 'ngx-md';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BlogPostComponent } from './blog-post/blog-post.component';
+import { BlogHomeComponent } from './blog-home/blog-home.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     BlogPostComponent,
+    BlogHomeComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgxMdModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
