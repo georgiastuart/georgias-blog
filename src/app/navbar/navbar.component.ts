45a0da8f1b88c437c7181e572dc650b9944d5fb5
Georgia Stuart <georgia.stuart@gmail.com>
2019-04-13 15:33:38 -0500

    changed photo to webp

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -10,7 +10,7 @@ export class NavbarComponent implements OnInit {
   imgHeight = 160;
   navHeight = 0;
   isCollapsed = true;
-  georgiaHeadshot = 'assets/images/georgia.png';
+  georgiaHeadshot = 'assets/images/georgia.webp';
   constructor() { }
 
   ngOnInit() {
